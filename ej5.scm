2abd48fbfb5c0e647edb41f1015399c5d58b4cb5
Francisco Reyes <freyes@alumnos.inf.utfsm.cl>
2016-10-27 18:52:41 -0300

    Ej5 casi listo, falta el determinante

--- ej5.scm
+++ ej5.scm
@@ -1,16 +1,28 @@
-(define F
-  (lambda (n x f L m g A)
-          (productoria 0 n x f L m g A)))
+#lang scheme
 
-(define (productoria i n x f L m g A )
-  (if (< i n)
-      (* (productoria (+ i 2) n x f L m g A)
-         (- (f (potencia x i)) (sumatoria ...)))
-      1))
-
-(define (sumatoria L m g))
+(define (F n x f L m g A)
+  (let func ((i 0) (num 1))
+    (if(equal? i n) num
+       (func (+ i 2) (* (- (f (potencia x i)) (sumatoria i L m g A)) num))
+       )
+    )
+  )
+        
+(define (sumatoria i L m g A)
+  (cond
+  ((null? L) 0)
+  ((< (car L) m)(+ (* (g (/ i (car L))) (potencia A (car L)))
+        (sumatoria i (cdr L) m g A)))
+  (else (sumatoria i (cdr L) m g A))))
 
 (define (potencia x y)
-  (if (= y 1)
-      x
-      (* x (potencia x (- y 1)))))
+  (cond
+    ((equal? y 0) 1)
+    ((equal? y 1) x)
+    (else 
+      (* x (potencia x (- y 1)))
+      )
+    )
+  )
+
+;(F 6 2 (lambda (x) (+ x x)) '(1 4 6 2 3) 3 (lambda (x) (* x 2)) -2)
