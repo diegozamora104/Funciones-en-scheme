5fa9b9cba3fcc1458c53aff8672434951fbb0b56
Francisco Reyes <freyes@alumnos.inf.utfsm.cl>
2016-11-03 14:14:14 -0300

    pre-orden

--- pre-orden.scm
+++ pre-orden.scm
@@ -10,16 +10,17 @@
   (cons
    (if (not(pair? arbol1)) 0
        (+ (car arbol1) (car arbol2)))
-   (if (not(pair? arbol1)) '()
+   (if (not(pair? arbol1)) 0
        (append (list (+ (car(pre-orden2 (izq arbol1) arbol2)) (car(pre-orden2 arbol1 (izq arbol2))))) 
                (list (+ (car(pre-orden2 (drch arbol1) arbol2)) (car(pre-orden2 arbol1 (drch arbol2)))))
                ))))
 
-(define (pre-orden arbol)
+(define (pre-orden arbol1 arbol2)
   (cons
-   (if (not(pair? arbol)) '()
-       (car arbol))
-   (if (not(pair? arbol)) '()
-       (append (pre-orden (izq arbol)) (pre-orden (drch arbol))))))
+   (if (and (not(pair? arbol1)) (not(pair? arbol2))) '()
+       (+ (car arbol1) (car arbol2)))
+   (if (and (not(pair? arbol1)) (not(pair? arbol2))) '()
+       (append (list (+ (car (pre-orden (izq arbol1) arbol2)) (car (pre-orden arbol1 (izq arbol2)))))
+               (list (+ (car (pre-orden (drch arbol1) arbol2)) (car (pre-orden drch (izq arbol2)))))))))
 
 ;(pre-orden '(1(2()())(3()())) '(1(2()())(3()())) )
