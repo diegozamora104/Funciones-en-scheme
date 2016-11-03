9aa811c11a638686d67d18052ffdb031d807f65a
Francisco Reyes <freyes@alumnos.inf.utfsm.cl>
2016-11-03 14:38:06 -0300

    ej4 listo

--- ej4.scm
+++ ej4.scm
@@ -1,22 +1,23 @@
-#lang racket
+#lang scheme
 
-(define branch
-  (lambda (tree fn final)
-    (if (not(equal? '() (cadr tree)));Izquierda
-        (branch (cadr tree) fn final))
-    (if (not(equal? '() (caddr tree)));Derecha
-        (branch (caddr tree) fn tree))
-    (cons (fn (car final) (car tree))
-    (cons (cons (fn (cadr final) (cadr tree)) '(()()))
-    (list (cons (fn (caddr final) (caddr tree)) '(()()) ))))
+(define operate-bintrees
+  (lambda (lsarboles funcion)
+    (let func ((base (car lsarboles))(arbol (cdr lsarboles))(op funcion))
+      (if (null? arbol) base
+        (func (nav (car arbol) base op) (cdr arbol) op )))))
 
 
+(define nav
+  (lambda (tree final fn)
+    (cons
+      (fn (car tree) (car final))
+      (append
+      (if (null? (cadr tree)) (list '());Left
+          (list (nav (cadr tree) (cadr final) fn)))
+      (if (null? (caddr tree)) (list '());Right
+          (list (nav (caddr tree) (caddr final) fn)))))
     ))
+      
 
-;Siempre es asi?: '(1 (2 ()() ) (3 ()()) )
-;o puede estar asi?: '(1 (2) (3))
-    
-
-
-
-    
+;(define trees '((1 (2 () ()) (3 () ())) (1 (1 () ()) (1 () ())) (7 (8() ()) (9 () ()))))
+;(operate-bintrees tress +)
